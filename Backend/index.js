345f3a82155d00642a9c6ceeb87bb430f59c0c37
Nilesh <thakren76@gmail.com>
2023-02-27 08:56:22 +0530

    create file in folder

--- Backend/index.js
+++ Backend/index.js
@@ -1,13 +1,6 @@
-const fs= require('fs');
-
-const  file= process.argv;
-
-if(file[2]==="add"){
-    fs.writeFileSync(file[3],file[4])
-
-    }else if(file[2]==="remove"){
-        fs.unlinkSync(file[3])
-    }
-    else{
-        console.log("invalid format")
-    }
+const fs=require("fs");
+const path=require("path");
+const dirpath=path.join(__dirname,"file");
+for(let i=0;i<5;i++){
+    fs.writeFileSync(`${dirpath}/profile${i}.text`,"hello")
+}
